d6a4ff029969022ab5f7f8e9df919e18713e5cef
SravanthiN4 <snedunoori44@gmail.com>
2022-05-13 12:54:44 -0500

    add changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,21 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header'
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
     
     <Router>
-   <Header/>
-
-  <main className='py-3'>
-    <Container>
-    <Routes> <Route path = '/' component = {HomeScreen} exact/></Routes>
-        <HomeScreen />
-       </Container>
+      <Header/>
+        <main className='py-3'>
+         <Container>
+           <Routes> 
+                <Route path = '/' element = { <HomeScreen/>} exact/>
+                <Route path = '/product/:id' element = { <ProductScreen/>} />
+            </Routes>
+    
+          </Container>
       </main>
       <Footer/>
      
